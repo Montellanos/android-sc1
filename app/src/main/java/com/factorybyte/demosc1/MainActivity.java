1aecfa4ae2c0bc5e2441aa6a45a0824ccb5bed83
Jorge Montellanos Díaz <jorgemontellanos@gmail.com>
2018-06-23 17:38:04 -0400

    un paso antes de los pagos

--- app/src/main/java/com/factorybyte/demosc1/MainActivity.java
+++ app/src/main/java/com/factorybyte/demosc1/MainActivity.java
@@ -1,10 +1,16 @@
 package com.factorybyte.demosc1;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +29,23 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView miimagen = findViewById(R.id.miimage);
 
+        ImageView photo = findViewById(R.id.photo);
+
         Button vermas = findViewById(R.id.vermas);
+
+        final TextView description = findViewById(R.id.description);
+
+        TextView vermas_texto = findViewById(R.id.vermas_texto);
+
+        int param = 145;
+        if (description.getText().length() > param){
+            String aux = description.getText().toString();
+            description.setText(aux.substring(0, param) + "...");
+
+            vermas_texto.setVisibility(View.VISIBLE);
+
+
+        }
 
 
         RequestOptions options = new RequestOptions();
@@ -37,6 +59,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        Glide
+                .with(this)
+                .load("https://images.pexels.com/photos/985635/pexels-photo-985635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+                .apply(options.fitCenter().centerCrop().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC))
+                .thumbnail(0.1f)
+                .into(photo);
+
+
+
         vermas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,6 +77,49 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        ImageButton llamar = findViewById(R.id.llamar);
+
+
+
+        llamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+                    Intent intent = new Intent(Intent.ACTION_CALL,  Uri.parse("tel:" + "75116755"));
+                    startActivity(intent);
+                }else{
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                }
+
+
+            }
+        });
+
+
+
+
+
+        ImageButton pagar = findViewById(R.id.pagar);
+
+
+        pagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(getApplicationContext(), PaymentActivity.class);
+                intent.putExtra("image", "https://images.pexels.com/photos/985635/pexels-photo-985635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+                intent.putExtra("product_title", getResources().getString(R.string.vestido));
+                intent.putExtra("precio", "150");
+                intent.putExtra("descripcion", description.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+
 
 
 
---
+++ app/src/main/java/com/factorybyte/demosc1/PaymentActivity.java
@@ -0,0 +1,68 @@
+package com.factorybyte.demosc1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+
+
+        String image = intent.getStringExtra("image");
+
+        String product_title = intent.getStringExtra("product_title");
+
+        String precio =  intent.getStringExtra("precio");
+
+        String description = intent.getStringExtra("descripcion");
+
+
+        getSupportActionBar().setTitle(product_title);
+
+
+        TextView price = findViewById(R.id.precio_texto);
+
+
+        price.setText(precio  + " Bs.-");
+
+
+        TextView descripcion_texto = findViewById(R.id.description_texto);
+
+        descripcion_texto.setText(description);
+
+
+        ImageView imagen_producto = findViewById(R.id.image_producto);
+
+
+        RequestOptions options = new RequestOptions();
+
+
+        Glide
+                .with(this)
+                .load(image)
+                .apply(options.fitCenter().centerCrop().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC))
+                .thumbnail(0.1f)
+                .into(imagen_producto);
+
+
+
+    }
+}
